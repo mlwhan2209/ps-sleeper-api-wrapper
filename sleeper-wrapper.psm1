76f31d51eca9b77886b00f6bf131161067ed3a3b
Matt Whan <mlwhan2209@gmail.com>
2021-12-29 21:39:23 -0500

    gitignore, SleeperUser function now using advanced function

--- sleeper-wrapper.psm1
+++ sleeper-wrapper.psm1
@@ -11,8 +11,47 @@ function Get-SleeperUser {
         $Uri = "$SleeperBaseUri/user/{username}"
     }
     Process {
-        # Create a temporary variable from the Uri variable contain int the PetId parameter
-        $TempUri = $Uri -replace "\{username\}", $UserName
+        try {
+            $TempUri = $Uri -replace "\{username\}", $UserName            
+            # Call the API endpoint using the $TempUri
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find anyone with that username"
+        }
+    }
+} 
+
+function Get-SleeperLeague {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID
+    )
+    Begin {
+        ## Using the module-scoped API endpoint URI to create a function-specific URL to query
+        $Uri = "$SleeperBaseUri/league/{league_id}"
+    }
+    Process {
+        $TempUri = $Uri -replace "\{league_id\}", $LeagueID
+            
+        # Call the API endpoint using the $TempUri
+        Invoke-RestMethod -Uri $TempUri -Method Get 
+    }
+} 
+
+function Get-SleeperLeagueRosters {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID
+    )
+    Begin {
+        ## Using the module-scoped API endpoint URI to create a function-specific URL to query
+        $Uri = "$SleeperBaseUri/league/{league_id}/rosters"
+    }
+    Process {
+        $TempUri = $Uri -replace "\{league_id\}", $LeagueID
             
         # Call the API endpoint using the $TempUri
         Invoke-RestMethod -Uri $TempUri -Method Get 
