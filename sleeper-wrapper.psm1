9e2f9144eb376d03108adcb17e6d2d937d70e4b7
Matt Whan <mlwhan2209@gmail.com>
2021-12-30 21:42:10 -0500

    finished importing league functions

--- sleeper-wrapper.psm1
+++ sleeper-wrapper.psm1
@@ -20,6 +20,26 @@ function Get-SleeperUser {
     }
 } 
 
+function Get-SleeperAllLeagues {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$UserID,[Int64]$Season
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/user/{user_id}/leagues/nfl/{season}"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{league_id\}", $LeagueID -replace "\{season\}", $Season
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find anyone with that userID in that season"
+        }
+    }
+}
+
 function Get-SleeperLeague {
     [cmdletbinding()]
     param(
@@ -40,6 +60,65 @@ function Get-SleeperLeague {
     }
 }
 
+function Get-SleeperLeagueRosters {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/league/{league_id}/rosters"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{league_id\}", $LeagueID
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find any league with that ID"
+        }
+    }
+}
+
+function Get-SleeperLeagueUsers {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/league/{league_id}/users"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{league_id\}", $LeagueID
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find any league with that ID"
+        }
+    }
+}
+
+function Get-SleeperLeagueMatchups {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID,[Int64]$Week
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/league/{league_id}/matchups/{week}"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{league_id\}", $LeagueID -replace "\{week\}", $Week
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find any league with that ID in that week"
+        }
+    }
+}
 
 function Get-SleeperLeagueRosters {
     [cmdletbinding()]
@@ -57,6 +136,106 @@ function Get-SleeperLeagueRosters {
         }
         catch {
             Write-Warning -Message "Unable to find anyone with that username"
+        }
+    }
+} 
+
+function Get-SleeperPlayoffWinnersBracket {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/league/{league_id}/winners_bracket"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{league_id\}", $LeagueID
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find any playoffs matchups"
+        }
+    }
+} 
+
+function Get-SleeperPlayoffLosersBracket {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/league/{league_id}/losers_bracket"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{league_id\}", $LeagueID
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find any playoffs matchups"
+        }
+    }
+} 
+
+function Get-SleeperLeagueTransactions {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID,[Int64]$Week
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/league/{league_id}/transactions/{week}"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{league_id\}", $LeagueID -replace "\{week\}",$Week
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find any transactions"
+        }
+    }
+} 
+
+function Get-SleeperLeagueTradedPicks {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Int64]$LeagueID
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/league/{league_id}/traded_picks"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{league_id\}", $LeagueID
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find any traded picks"
+        }
+    }
+} 
+
+function Get-SleeperSportInfo {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]$Sport
+    )
+    Begin {
+        $Uri = "$SleeperBaseUri/state/{sport}"
+    }
+    Process {
+        try {
+            $TempUri = $Uri -replace "\{sport\}", $Sport
+            Invoke-RestMethod -Uri $TempUri -Method Get -ErrorAction Stop
+        }
+        catch {
+            Write-Warning -Message "Unable to find any info"
         }
     }
 } 
